2dbcc7d2f9521151ca13e420dd7b49aa6434daa5
rochoa85 <raod85@hotmail.com>
2013-03-14 21:22:15 -0500

    Add acknowledgement

--- app/acknowledgements.php
+++ app/acknowledgements.php
@@ -23,7 +23,8 @@ URL: http://www.opensource.org/licenses/apache2.0.php
                <li><a href="http://ora2pg.darold.net/">Ora2Pg project</a> - Gilles Darold</li>
    		       <li><a href="http://www.rdkit.org">RDKit</a> - Greg Landrum</li>
    		       <li><a href="http://www.chemaxon.com/products/marvin/marvinsketch/">Marvin Sketch</a> - ChemAxon</li>
-   		       <li><a href="https://www.ebi.ac.uk/chembl">ChEMBL Group</a> - EMBL-EBI</li> 	
+   		       <li><a href="https://www.ebi.ac.uk/chembl">ChEMBL Group</a> - EMBL-EBI</li>
+		       <li><a href="http://www.linkedin.com/pub/rodrigo-alonso-ochoa-deossa/b/817/638">Rodrigo Ochoa</a></li> 	
     		 </ul>   			
 		</section> 
 			

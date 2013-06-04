1432196db727d164ddb4b23ade57df42f0677d7d
rochoa85 <raod85@hotmail.com>
2013-06-04 21:33:05 +0200

    Change the configuration data

--- config/config.php
+++ config/config.php
@@ -19,10 +19,12 @@ URL: http://www.opensource.org/licenses/apache2.0.php
   $app_title = "Open ChEMBL";
   $basedir   = "/var/www/openchembl/";
   $app2base  = "../";
+  $tmpdir         = "/tmp";
+  $chembl_version = "ChEMBL_16";
   
   # Database Settings
   $db_user = "user"; 
-  $db_name = "chembl_15";
+  $db_name = "chembl_16";
   $db_host = "/var/run/postgresql";
   $db_port = 5432;  
 ?>

502472e59bfbecde8acdac3b55b1c76eb0c50810
rochoa85 <raod85@hotmail.com>
2013-07-22 17:08:59 +0200

    Change in the acknowledgements section

--- app/acknowledgements.php
+++ app/acknowledgements.php
@@ -22,7 +22,7 @@ URL: http://www.opensource.org/licenses/apache2.0.php
              <ul>
                <li><a href="http://ora2pg.darold.net/">Ora2Pg project</a> - Gilles Darold</li>
    		       <li><a href="http://www.rdkit.org">RDKit</a> - Greg Landrum</li>
-   		       <li><a href="http://www.chemaxon.com/products/marvin/marvinsketch/">Marvin Sketch</a> - ChemAxon</li>
+   		       <li><a href="http://peter-ertl.com/jsme/">JSME</a> - Peter Ertl</li>
    		       <li><a href="https://www.ebi.ac.uk/chembl">ChEMBL Group</a> - EMBL-EBI</li>
 		       <li><a href="http://www.linkedin.com/pub/rodrigo-alonso-ochoa-deossa/b/817/638">Rodrigo Ochoa</a></li> 	
     		 </ul>   			
